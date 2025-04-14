07c991636d309328a4aa7bcddcbccf94f1e2e447
NguyenTongAnhQuan443 <ntanhquan.sly@gmail.com>
2025-04-14 21:40:55 +0700

    Update config firebase push notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,66 @@
 import 'package:flutter/material.dart';
-import 'package:olachat_mobile/ui/views/splash_screen.dart';
-import 'package:olachat_mobile/view_models/forgot_password_view_model.dart';
-import 'package:olachat_mobile/view_models/login_view_model.dart';
-import 'package:olachat_mobile/view_models/phone_verification_view_model.dart';
-import 'package:olachat_mobile/view_models/profile_view_model.dart';
-import 'package:olachat_mobile/view_models/reset_password_view_model.dart';
-import 'package:olachat_mobile/view_models/search_view_model.dart';
-import 'package:olachat_mobile/view_models/signup_view_model.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+import 'firebase_options.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-
 import 'core/utils/app_lifecycle_handler.dart';
+import 'ui/views/splash_screen.dart';
+import 'view_models/login_view_model.dart';
+import 'view_models/forgot_password_view_model.dart';
+import 'view_models/phone_verification_view_model.dart';
+import 'view_models/reset_password_view_model.dart';
+import 'view_models/signup_view_model.dart';
+import 'view_models/search_view_model.dart';
+import 'view_models/profile_view_model.dart';
 
-final GlobalKey<ScaffoldMessengerState> scaffoldMessengerKey = GlobalKey<ScaffoldMessengerState>();
+// GLOBAL
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+final GlobalKey<ScaffoldMessengerState> scaffoldMessengerKey =
+    GlobalKey<ScaffoldMessengerState>();
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
-final AppLifecycleHandler _lifecycleHandler = AppLifecycleHandler();
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+}
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final AppLifecycleHandler lifecycleHandler = AppLifecycleHandler();
   WidgetsBinding.instance.addObserver(lifecycleHandler);
-  print("📲 AppLifecycleHandler added");
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  // Cấu hình hiển thị local notification
+  const androidSettings = AndroidInitializationSettings('@mipmap/ic_launcher');
+  const initSettings = InitializationSettings(android: androidSettings);
+  await flutterLocalNotificationsPlugin.initialize(initSettings);
+  // Khi app đang mở
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    if (message.notification != null) {
+      flutterLocalNotificationsPlugin.show(
+        message.hashCode,
+        message.notification!.title,
+        message.notification!.body,
+        NotificationDetails(
+          android: AndroidNotificationDetails(
+            'default_channel',
+            'Thông báo',
+            importance: Importance.max,
+            priority: Priority.high,
+          ),
+        ),
+      );
+    }
+  });
+
+  FirebaseMessaging.instance.getToken().then((token) {
+    print("📱 FCM Token: $token");
+  });
 
   runApp(
     MultiProvider(
@@ -32,23 +72,21 @@ Future<void> main() async {
         ChangeNotifierProvider(create: (_) => ResetPasswordViewModel()),
         ChangeNotifierProvider(create: (_) => ProfileViewModel()),
         ChangeNotifierProvider(create: (_) => SearchViewModel()),
-
       ],
-      child: MyApp(),
+      child: const MyApp(),
     ),
   );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       scaffoldMessengerKey: scaffoldMessengerKey,
       navigatorKey: navigatorKey,
-      title: 'OlaChat',
       debugShowCheckedModeBanner: false,
+      title: 'OlaChat',
       home: const SplashScreen(),
       localizationsDelegates: const [
         GlobalMaterialLocalizations.delegate,

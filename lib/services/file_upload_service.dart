154334b00c693d2be48e1921c7c8360d4d7096c5
ADMIN <ntanhquan.sly@gmail.com>
2025-05-30 14:11:48 +0700

    update upload file

--- lib/services/file_upload_service.dart
+++ lib/services/file_upload_service.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 import 'package:dio/dio.dart';
+import 'package:olachat_mobile/services/token_service.dart';
+import 'package:olachat_mobile/utils/app_styles.dart';
 import '../config/api_config.dart';
 import 'dio_client.dart';
 
@@ -7,21 +9,36 @@ class FileUploadService {
   final Dio _dio = DioClient().dio;
 
   Future<List<String>> uploadFiles(List<File> files) async {
-    final formData = FormData();
+    try {
+      final accessToken = await TokenService.getAccessToken(); // 👈 Lấy token
 
-    for (final file in files) {
-      formData.files.add(MapEntry(
-        'files',
-        await MultipartFile.fromFile(file.path, filename: file.path.split('/').last),
-      ));
+      final formData = FormData();
+
+      for (final file in files) {
+        formData.files.add(MapEntry(
+          'files',
+          await MultipartFile.fromFile(
+            file.path,
+            filename: file.path.split('/').last,
+          ),
+        ));
+      }
+
+      final response = await _dio.post(
+        ApiConfig.uploadFileMobile,
+        data: formData,
+        options: Options(
+          headers: {
+            'Authorization': 'Bearer $accessToken',
+          },
+        ),
+      );
+
+      final List<dynamic> uploaded = response.data['files'];
+      return uploaded.map((e) => e['fileUrl'] as String).toList();
+    } catch (e) {
+      print("${AppStyles.failureIcon} [Upload] Lỗi upload file: $e");
+      rethrow;
     }
-
-    final response = await _dio.post(
-      ApiConfig.uploadFileMobile, // endpoint bạn cung cấp
-      data: formData,
-    );
-
-    final List<dynamic> uploaded = response.data['files'];
-    return uploaded.map((e) => e['fileUrl'] as String).toList();
   }
 }
